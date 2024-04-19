5ec42d5715ecaf836d271552409a7475fdf5e477
Lance <71721648+slicer229@users.noreply.github.com>
2024-04-18 19:31:05 -0600

    Amit and I built a start to main-view.fxml; renamed some files

--- Workshop 6 JavaFX/src/main/java/org/example/workshop6javafx/MainController.java
+++ Workshop 6 JavaFX/src/main/java/org/example/workshop6javafx/MainController.java
@@ -3,7 +3,7 @@ package org.example.workshop6javafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
