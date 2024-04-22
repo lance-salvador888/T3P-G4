3de9a4742bf7fec350f117c04fe9ac9ea568d1cd
Lance <71721648+slicer229@users.noreply.github.com>
2024-04-22 03:34:30 -0600

    Updated libraries, Made Booking.java, updated the TreeTableView to only have one column (multi-column won't work for what we're trying to do)

---
+++ Workshop 6 JavaFX/src/main/java/org/example/workshop6javafx/Booking.java
@@ -0,0 +1,97 @@
+package org.example.workshop6javafx;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Booking {
+    private SimpleIntegerProperty bookingId;
+    private SimpleIntegerProperty bookingDate;
+    private SimpleStringProperty bookingNo;
+    private SimpleIntegerProperty bookingTravelerCount;
+    private SimpleIntegerProperty bookingCustomerId;
+    private SimpleStringProperty bookingTripTypeId;
+    // private SimpleIntegerProperty PackageId;
+    // ^commenting this out rn cause idk if it's gonna get used
+
+    public Booking(int bookingId, int bookingDate, String bookingNo,
+                   int travelerCount, int customerId, String tripTypeId ) {
+        this.bookingId = new SimpleIntegerProperty(bookingId);
+        this.bookingDate = new SimpleIntegerProperty(bookingDate);
+        this.bookingNo = new SimpleStringProperty(bookingNo);
+        this.bookingTravelerCount = new SimpleIntegerProperty(travelerCount);
+        this.bookingCustomerId = new SimpleIntegerProperty(customerId);
+        this.bookingTripTypeId = new SimpleStringProperty(tripTypeId);
+    }
+
+    public int getBookingId() {
+        return bookingId.get();
+    }
+
+    public SimpleIntegerProperty bookingIdProperty() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId.set(bookingId);
+    }
+
+    public int getBookingDate() {
+        return bookingDate.get();
+    }
+
+    public SimpleIntegerProperty bookingDateProperty() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(int bookingDate) {
+        this.bookingDate.set(bookingDate);
+    }
+
+    public String getBookingNo() {
+        return bookingNo.get();
+    }
+
+    public SimpleStringProperty bookingNoProperty() {
+        return bookingNo;
+    }
+
+    public void setBookingNo(String bookingNo) {
+        this.bookingNo.set(bookingNo);
+    }
+
+    public int getBookingTravelerCount() {
+        return bookingTravelerCount.get();
+    }
+
+    public SimpleIntegerProperty bookingTravelerCountProperty() {
+        return bookingTravelerCount;
+    }
+
+    public void setBookingTravelerCount(int bookingTravelerCount) {
+        this.bookingTravelerCount.set(bookingTravelerCount);
+    }
+
+    public int getBookingCustomerId() {
+        return bookingCustomerId.get();
+    }
+
+    public SimpleIntegerProperty bookingCustomerIdProperty() {
+        return bookingCustomerId;
+    }
+
+    public void setBookingCustomerId(int bookingCustomerId) {
+        this.bookingCustomerId.set(bookingCustomerId);
+    }
+
+    public String getBookingTripTypeId() {
+        return bookingTripTypeId.get();
+    }
+
+    public SimpleStringProperty bookingTripTypeIdProperty() {
+        return bookingTripTypeId;
+    }
+
+    public void setBookingTripTypeId(String bookingTripTypeId) {
+        this.bookingTripTypeId.set(bookingTripTypeId);
+    }
+}
--- Workshop 6 JavaFX/src/main/java/org/example/workshop6javafx/MainController.java
+++ Workshop 6 JavaFX/src/main/java/org/example/workshop6javafx/MainController.java
@@ -1,14 +1,68 @@
+/**
+ * Sample Skeleton for 'main-view.fxml' Controller Class
+ */
+
 package org.example.workshop6javafx;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnAdd"
+    private Button btnAdd; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnDelete"
+    private Button btnDelete; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnEdit"
+    private Button btnEdit; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lcTravelerGraph"
+    private LineChart<?, ?> lcTravelerGraph; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lcXDate"
+    private CategoryAxis lcXDate; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lcYTravelerCount"
+    private NumberAxis lcYTravelerCount; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ttvBookings"
+    private TreeTableView<Booking> ttvBookings; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ttvColBooking"
+    private TreeTableColumn<Booking, String> ttvColBooking; // Value injected by FXMLLoader
+
+    @FXML // fx:id="welcomeText"
+    private Label welcomeText; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnAdd != null : "fx:id=\"btnAdd\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert btnDelete != null : "fx:id=\"btnDelete\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert btnEdit != null : "fx:id=\"btnEdit\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert lcTravelerGraph != null : "fx:id=\"lcTravelerGraph\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert lcXDate != null : "fx:id=\"lcXDate\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert lcYTravelerCount != null : "fx:id=\"lcYTravelerCount\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert ttvBookings != null : "fx:id=\"ttvBookings\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert ttvColBooking != null : "fx:id=\"ttvColBooking\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert welcomeText != null : "fx:id=\"welcomeText\" was not injected: check your FXML file 'main-view.fxml'.";
+
+
     }
+
 }
