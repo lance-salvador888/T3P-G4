5630e7d9c70b6969d136c3d9d4c4b6c1b7f67ec0
Amit <110861563+AmitShalev0@users.noreply.github.com>
2024-04-24 10:43:12 -0600

    updated gluon stuff. 1043 april 24

--- Workshop 6 JavaFX/src/main/java/org/example/workshop6javafx/ChartController.java
+++ Workshop 6 JavaFX/src/main/java/org/example/workshop6javafx/ChartController.java
@@ -1,4 +1,11 @@
+/**
+ * Sample Skeleton for 'chart.fxml' Controller Class
+ */
+
 package org.example.workshop6javafx;
+
+import com.gluonhq.charm.glisten.control.Icon;
+import com.gluonhq.charm.glisten.control.ToggleButtonGroup;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,10 +14,8 @@ import java.sql.*;
 import java.util.Properties;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.StackedBarChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
+import javafx.scene.layout.AnchorPane;
 
 public class ChartController {
 
@@ -20,8 +25,17 @@ public class ChartController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="apChart"
+    private AnchorPane apChart; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lcCategoryAxis"
+    private CategoryAxis lcCategoryAxis; // Value injected by FXMLLoader
+
     @FXML // fx:id="lcLineChart"
     private LineChart<?, ?> lcLineChart; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lcNumberAxis"
+    private NumberAxis lcNumberAxis; // Value injected by FXMLLoader
 
     @FXML // fx:id="pcPieChart"
     private PieChart pcPieChart; // Value injected by FXMLLoader
@@ -29,11 +43,40 @@ public class ChartController {
     @FXML // fx:id="sbcBarChart"
     private StackedBarChart<?, ?> sbcBarChart; // Value injected by FXMLLoader
 
+    @FXML // fx:id="sbcCategoryAxis"
+    private CategoryAxis sbcCategoryAxis; // Value injected by FXMLLoader
+
+    @FXML // fx:id="sbcNumberAxis"
+    private NumberAxis sbcNumberAxis; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tbBar"
+    private Icon tbBar; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tbLine"
+    private Icon tbLine; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tbPie"
+    private Icon tbPie; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tbgCharts"
+    private ToggleButtonGroup tbgCharts; // Value injected by FXMLLoader
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert apChart != null : "fx:id=\"apChart\" was not injected: check your FXML file 'chart.fxml'.";
+        assert lcCategoryAxis != null : "fx:id=\"lcCategoryAxis\" was not injected: check your FXML file 'chart.fxml'.";
         assert lcLineChart != null : "fx:id=\"lcLineChart\" was not injected: check your FXML file 'chart.fxml'.";
+        assert lcNumberAxis != null : "fx:id=\"lcNumberAxis\" was not injected: check your FXML file 'chart.fxml'.";
         assert pcPieChart != null : "fx:id=\"pcPieChart\" was not injected: check your FXML file 'chart.fxml'.";
         assert sbcBarChart != null : "fx:id=\"sbcBarChart\" was not injected: check your FXML file 'chart.fxml'.";
+        assert sbcCategoryAxis != null : "fx:id=\"sbcCategoryAxis\" was not injected: check your FXML file 'chart.fxml'.";
+        assert sbcNumberAxis != null : "fx:id=\"sbcNumberAxis\" was not injected: check your FXML file 'chart.fxml'.";
+        assert tbBar != null : "fx:id=\"tbBar\" was not injected: check your FXML file 'chart.fxml'.";
+        assert tbLine != null : "fx:id=\"tbLine\" was not injected: check your FXML file 'chart.fxml'.";
+        assert tbPie != null : "fx:id=\"tbPie\" was not injected: check your FXML file 'chart.fxml'.";
+        assert tbgCharts != null : "fx:id=\"tbgCharts\" was not injected: check your FXML file 'chart.fxml'.";
+
+
 
     }
     public void populateLineChart() {
