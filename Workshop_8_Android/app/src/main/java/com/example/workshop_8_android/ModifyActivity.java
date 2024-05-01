ecc6e35d5ff9a07f803a3131bc004a56658fdb01
Amit <110861563+AmitShalev0@users.noreply.github.com>
2024-04-30 19:06:27 -0600

    making a ModifyActivity and activity_modify.xml. page on the app (workshop 8)

---
+++ Workshop_8_Android/app/src/main/java/com/example/workshop_8_android/ModifyActivity.java
@@ -0,0 +1,13 @@
+package com.example.workshop_8_android;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ModifyActivity extends AppCompatActivity{
+
+
+}
