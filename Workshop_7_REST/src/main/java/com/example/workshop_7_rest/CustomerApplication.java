2a49e007ec51fefb7688597be8bace35c56dce59
Lance <lance.salvador888@gmail.com>
2024-05-06 06:16:55 -0600

    deleted duped REST folder

--- Workshop_7_REST/src/main/java/com/example/workshop_7_rest/CustomerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.workshop_7_rest;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class CustomerApplication extends Application {
-
-}
